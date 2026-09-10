2e3c7c619d783c21f4f4d26ed7eec486b67b19bb
Raphael Scharf Passos <raphaspassos@gmail.com>
2026-09-09 21:25:38 -0300

    adiciona funcoes de rolagem de dados

---
+++ Utils/RolagemDados.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OldDragonCriacao.Utils;
+
+public static class RolagemDados
+{
+    private static readonly Random _rnd = new();
+
+    public static int RolarDado(int lados = 6) => _rnd.Next(1, lados + 1);
+
+    public static int Rolar3d6()
+    {
+        return RolarDado() + RolarDado() + RolarDado();
+    }
+
+    public static int Rolar4d6DescartandoMenor()
+    {
+        var dados = new List<int> { RolarDado(), RolarDado(), RolarDado(), RolarDado() };
+        dados.Sort();
+        dados.RemoveAt(0); // descarta o menor dado
+        return dados.Sum();
+    }
+}
